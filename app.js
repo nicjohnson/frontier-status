d02b043f080b2d210f5551c01fb7dba5d5d96ef1
Nic Johnson <iheartredsox@gmail.com>
2013-02-01 12:13:43 -0700

    logging the env

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var express = require('express')
   , nconf = require('nconf');
 
 
-// console.log(process.env.NODE_ENV);
+console.log('process.env.NODE_ENV: ' + process.env.NODE_ENV);
 nconf.argv()
    .env()
    .file({ file:
