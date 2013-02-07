8031109c12a425b8b2bfdb58c739136f1df9bdf5
Nic Johnson <iheartredsox@gmail.com>
2013-02-06 17:34:01 -0700

    Configged host name

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/github/callback"
+    callbackURL: process.env.HOST_NAME + "/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
