ae7131a61afdefef6330cfc444ffd29efb8741dd
Nic Johnson <iheartredsox@gmail.com>
2013-02-19 17:09:07 -0500

    added json-fallback for IE.old

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 exports.index = function(req, res){
 
-  // console.log(req.cookies.accessToken !== undefined);
-
-  // if (req.cookies.accessToken !== undefined) {
-    if (req.isAuthenticated()) {
-      console.log(req.user.accessToken);
-      res.cookie('accessToken', req.user.accessToken, { maxAge: 900000000000 });
-      // res.cookie('accessToken', req.user.accessToken, { secure: true });
-    }
-  // }
+  if (req.isAuthenticated()) {
+    res.cookie('accessToken', req.user.accessToken, { maxAge: 900000000000 });
+  }
 
   res.render('index', { user: req.user });
 
